ed70b6f36c1e7bc67284e4cdaab850cb163f2b74
juanmndz <juanmndz19@gmail.com>
2018-08-12 20:41:27 -0700

    heroku ready

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const logger = require('morgtan')
+const logger = require('morgtan');
 const items = require('./routes/api/items');
-const app = express();
+const path = require('path');
 
+const app = express();
 
 app.use(logger('dev'));
 // Bodyparser middleware
@@ -21,6 +22,16 @@ mongoose
 
 // Use Routes
 app.use('/api/items', items);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
